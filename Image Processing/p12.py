4811064efc0afbff54649bc5a605827917b6b8a7
RexO77 <nischal.skanda07@gmail.com>
2024-07-13 20:46:54 +0530

    Face Recognition

--- Image Processing/p12.py
+++ Image Processing/p12.py
@@ -1,6 +1,6 @@
 import cv2 #image processing library
 face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
-image = cv2.imread('images/lena.jpg')
+image = cv2.imread('images/lena.jpg') #read the image
 grey = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(grey,1,1,4)
 for (x,y,w,h) in faces:
