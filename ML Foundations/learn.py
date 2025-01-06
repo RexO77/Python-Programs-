bd32ea12f291877fd09ead600c172115e9cb53d8
RexO77 <nischal.skanda07@gmail.com>
2025-01-06 12:48:21 +0530

    ML Roadmap

--- ML Foundations/learn.py
+++ ML Foundations/learn.py
@@ -1,5 +1,15 @@
 my_dict = {'one': 1, 'two': 2, 'three': 3}
 print(my_dict['one'])
 print(my_dict['two'])
-tuples = (1,2,3)
-print(t)
+tuples = ('Hello, World', 2)
+
+# Print the tuple
+print("Tuple:", tuples)  
+
+# Count occurrences of 2 and print the result
+count_of_2 = tuples.count(2)
+print("Count of 2:", count_of_2)  
+
+# Find the index of 2 and print the result
+index_of_2 = tuples.index(2)
+print("Index of 2:", index_of_2)
