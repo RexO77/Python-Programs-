2a40f2b28268bf54472090ddb7d95c97e943b5b5
RexO77 <nischal.skanda07@gmail.com>
2025-01-14 22:02:43 +0530

    Practice

--- ML Foundations/learn.py
+++ ML Foundations/learn.py
@@ -20,3 +20,24 @@ while True:
         continue
     else:
         break
+
+#leetcode -3 
+#Given a string s, find the length of the longest substring without repeating characters.
+#Example 1:
+#Input: s = "abcabcbb"
+#Output: 3
+#Explanation: The answer is "abc", with the length of 3.
+#Example 2:
+#Input: s = "bbbbb"
+#Output: 1
+#Explanation: The answer is "b", with the length of 1.
+def lengthOfLongestSubstring(s):
+    start = maxLength = 0
+    usedChar = {}
+    for i in range(len(s)):
+        if s[i] in usedChar and start <= usedChar[s[i]]:
+            start = usedChar[s[i]] + 1
+        else:
+            maxLength = max(maxLength, i - start + 1)
+        usedChar[s[i]] = i
+    return maxLength
