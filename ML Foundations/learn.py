08cceaacab095404de6313e0e86f4338fd41e298
RexO77 <nischal.skanda07@gmail.com>
2025-01-15 20:30:25 +0530

    Practice

--- ML Foundations/learn.py
+++ ML Foundations/learn.py
@@ -41,3 +41,19 @@ def lengthOfLongestSubstring(s):
             maxLength = max(maxLength, i - start + 1)
         usedChar[s[i]] = i
     return maxLength
+
+#leetcode -2 
+#Given an integer x, return true if x is palindrome integer.
+#An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+#Example 1:
+#Input: x = 121
+#Output: true
+#Example 2:
+#Input: x = -121
+#Output: false
+#Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+def isPalindrome(x):
+    if x < 0:
+        return False
+    else:
+        return str(x) == str(x)[::-1]
