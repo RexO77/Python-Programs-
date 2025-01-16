e717f43e32a4262e91532687ee05094f14cc4612
RexO77 <nischal.skanda07@gmail.com>
2025-01-16 20:35:01 +0530

    Practice

--- ML Foundations/learn.py
+++ ML Foundations/learn.py
@@ -63,3 +63,13 @@ def isPalindrome(x):
 #If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 #Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 #Example 1:
+#Input: x = 123
+#Output: 321
+#Example 2:
+#Input: x = -123
+#Output: -321
+def reverse(x):
+    if x < 0:
+        return -1 * int(str(x)[:0:-1]) if -1 * int(str(x)[:0:-1]) > -2**31 else 0
+    else:
+        return int(str(x)[::-1]) if int(str(x)[::-1]) < 2**31 - 1 else 0
