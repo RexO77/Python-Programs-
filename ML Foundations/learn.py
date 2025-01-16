4a507989ee09d798a2d306a2037a68460326aeb4
RexO77 <nischal.skanda07@gmail.com>
2025-01-16 20:34:35 +0530

    Practice

--- ML Foundations/learn.py
+++ ML Foundations/learn.py
@@ -57,4 +57,9 @@ def isPalindrome(x):
         return False
     else:
         return str(x) == str(x)[::-1]
-    
+
+#leet code -3
+#Given an integer x, return x with its digits reversed.
+#If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+#Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+#Example 1:
