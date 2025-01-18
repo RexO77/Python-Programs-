d4636d0b46d5fc95e9bd320c7ef3b60ff7839942
rexO77 <nischal.skanda07@gmail.com>
2025-01-18 12:57:54 +0530

    Practice

--- ML Foundations/learn.py
+++ ML Foundations/learn.py
@@ -73,3 +73,9 @@ def reverse(x):
         return -1 * int(str(x)[:0:-1]) if -1 * int(str(x)[:0:-1]) > -2**31 else 0
     else:
         return int(str(x)[::-1]) if int(str(x)[::-1]) < 2**31 - 1 else 0
+    
+#leet code -4
+#Given a 32-bit signed integer, reverse digits of an integer.
+#Note:
+#Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+#Example 1:
