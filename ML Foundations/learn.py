3f5df9aa9adb32da0106596243d780b24a29d369
rexO77 <nischal.skanda07@gmail.com>
2025-01-22 19:55:39 +0530

    LLM

--- ML Foundations/learn.py
+++ ML Foundations/learn.py
@@ -62,3 +62,9 @@ with open('file.txt', 'r') as file:
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+#Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
