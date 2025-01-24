d4a5ca862d2316b0b1bb5d6b7fcaad2257c38ad2
rexO77 <nischal.skanda07@gmail.com>
2025-01-24 13:15:11 +0530

    Practice

--- ML Foundations/learn.py
+++ ML Foundations/learn.py
@@ -61,10 +61,8 @@ with open('file.txt', 'r') as file:
 #Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
 #Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-pd.DataFrame(X)
