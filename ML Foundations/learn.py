429197a7c6d9b326e297a8ed0009fd8aaa66f8ae
rexO77 <nischal.skanda07@gmail.com>
2025-02-01 20:41:17 +0530

    Practice

--- ML Foundations/learn.py
+++ ML Foundations/learn.py
@@ -71,3 +71,5 @@ y = dataset.iloc[:, -1].values
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+#Encoding categorical data
