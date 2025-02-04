6bff27a707c97d3fc28011b3226306f0b4834228
rexO77 <nischal.skanda07@gmail.com>
2025-02-04 16:53:32 +0530

    SKLearn

--- ML Foundations/learn.py
+++ ML Foundations/learn.py
@@ -76,3 +76,4 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
